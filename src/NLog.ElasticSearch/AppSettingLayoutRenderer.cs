751c5842dcc777d8dd84085d52efca4a75f74552
David Findley <xpando@gmail.com>
2012-12-03 10:56:56 -0600

    Initial commit

---
+++ src/NLog.ElasticSearch/AppSettingLayoutRenderer.cs
@@ -0,0 +1,46 @@
+﻿using System.Configuration;
+using System.Linq;
+using System.Text;
+using NLog.Config;
+using NLog.LayoutRenderers;
+
+namespace NLog.ElasticSearch
+{
+    [LayoutRenderer("appsetting")]
+    public sealed class AppSettingLayoutRenderer : LayoutRenderer
+    {
+        [RequiredParameter]
+        public string Key { get; set; }
+
+        public string Suffix { get; set; }
+
+        protected override void Append(StringBuilder builder, LogEventInfo logEvent)
+        {
+            if (!string.IsNullOrEmpty(this.Suffix))
+            {
+                var suffixParts = this.Suffix.Split(',');
+                for (var i = suffixParts.Length; i > 0; i--)
+                {
+                    var keyBuilder = new StringBuilder(this.Key, 140);
+                    foreach (var part in suffixParts.Take(i))
+                    {
+                        var partValue = ConfigurationManager.AppSettings[part.Trim()];
+                        if (!string.IsNullOrEmpty(partValue))
+                        {
+                            keyBuilder.Append(".");
+                            keyBuilder.Append(partValue);
+                        }
+                    }
+                    var value = ConfigurationManager.AppSettings[keyBuilder.ToString()];
+                    if (!string.IsNullOrEmpty(value))
+                    {
+                        builder.Append(value);
+                        return;
+                    }
+                }
+            }
+
+            builder.Append(ConfigurationManager.AppSettings[this.Key]);
+        }
+    }
+}
---
+++ src/NLog.ElasticSearch/ElasticSearchTarget.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using NLog.Common;
+using NLog.Config;
+using NLog.Layouts;
+using NLog.Targets;
+
+namespace NLog.ElasticSearch
+{
+    [Target("elasticsearch")]
+    public sealed class ElasticSearchTarget : Target
+    {
+        [RequiredParameter]
+        public Layout Url { get; set; }
+
+        [RequiredParameter]
+        public Layout Layout { get; set; }
+
+        protected override void Write(AsyncLogEventInfo info)
+        {
+            try
+            {
+                var url    = new Uri(this.Url.Render(info.LogEvent));
+                var layout = this.Layout.Render(info.LogEvent);
+                var json   = JObject.Parse(layout).ToString(); // make sure the json is valid
+                var client = new WebClient();
+
+                client.Headers.Add(HttpRequestHeader.ContentType, "application/json");
+
+                client.UploadStringCompleted += (s, e) =>
+                {
+                    if (e.Error != null)
+                    {
+                        if (e.Error is WebException)
+                        {
+                            var we = e.Error as WebException;
+                            try
+                            {
+                                var result = JObject.Load(new JsonTextReader(new StreamReader(we.Response.GetResponseStream())));
+                                var error = result.GetValue("error");
+                                if (error != null)
+                                {
+                                    info.Continuation(new Exception(result.ToString(), e.Error));
+                                    return;
+                                }
+                            }
+                            catch (Exception) { info.Continuation(new Exception("Failed to send log event to ElasticSearch", e.Error)); }
+                        }
+
+                        info.Continuation(e.Error);
+
+                        return;
+                    }
+
+                    info.Continuation(null);
+                };
+
+                client.UploadStringAsync(url, "PUT", json);
+            }
+            catch (Exception ex)
+            {
+                info.Continuation(ex);
+            }
+        }
+
+        protected override void Write(LogEventInfo logEvent)
+        {
+            throw new NotImplementedException();
+        }
+    } 
+}
